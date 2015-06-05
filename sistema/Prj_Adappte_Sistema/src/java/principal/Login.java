8f9f22d18ffd4a4b3dd65e3c78a34c4849befe70
PdroReis <pdroreis@gmail.com>
2015-06-05 05:40:51 -0300

    05/06/2015

---
+++ sistema/Prj_Adappte_Sistema/src/java/principal/Login.java
@@ -0,0 +1,17 @@
+package principal;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author MacBook
+ */
+@ManagedBean
+public class Login {
+    private String password;  
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+}
